04ba81cae4197e0ca82ee73d09a4eab8663b72d5
Kumar Gaurav <kumar.gaurav@synerzip.com>
2016-07-26 14:12:17 +0530

    Initial commit

--- README.md
+++ README.md
@@ -6,20 +6,19 @@ Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 
 There are two methods for getting started with this repo.
 
-####Familiar with Git?#####
+####Setup#####
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
-	> git clone git@github.com:StephenGrider/ReduxSimpleStarter.git
+	> git clone git@github.com:Gauravkr/ReduxSimpleStarter.git
 	> cd ReduxSimpleStarter
 	> npm install
 	> npm start
 ```
 
-####Not Familiar with Git?#####
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
 ```
 	> npm install
 	> npm start
 ```
+
+Thanks to 'StephenGrider' for his awesome tutorial.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyAxwGcPtS_DDDCYWjN7IEE4RMowrFoXzLY';
+
+//Create a new component and this component should produce some html
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('pokemonGo');
+    }
+
+    videoSearch(term){
+        YTSearch({ key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });// since the key and property have same name;
+        });
+    }
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+//take this component html and put in the DOM
+ReactDom.render(<App />, document.querySelector('.container'));
